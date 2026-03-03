25d3c118bda66c11f6ee7a5d2a872f48d780f945
Asif Shaik <sk.asif0490@gmail.com>
2026-03-03 17:48:02 +0530

    03-03-2026

--- app/Services/Messenger360Service.php
+++ app/Services/Messenger360Service.php
@@ -22,48 +22,7 @@ class Messenger360Service
      * @param string|null $mediaUrl
      * @param string|null $delay   (MM-DD-YYYY HH:MM in GMT)
      */
-    // public function send(
-    //     string $phone,
-    //     string $text,
-    //     ?string $mediaUrl = null,
-    //     ?string $delay = null
-    // ): array {
-    //     $postFields = [
-    //         'phonenumber' => $phone,
-    //         'text'        => $text,
-    //     ];
-
-    //     if ($mediaUrl) {
-    //         $postFields['url'] = $mediaUrl;
-    //     }
-
-    //     if ($delay) {
-    //         $postFields['delay'] = $delay;
-    //     }
-
-    //     $ch = curl_init();
-
-    //     curl_setopt_array($ch, [
-    //         CURLOPT_URL            => $this->url,
-    //         CURLOPT_RETURNTRANSFER => true,
-    //         CURLOPT_CUSTOMREQUEST  => 'POST',
-    //         CURLOPT_POSTFIELDS     => $postFields,
-    //         CURLOPT_HTTPHEADER     => [
-    //             'Authorization: Bearer ' . $this->token,
-    //         ],
-    //         CURLOPT_TIMEOUT        => 30,
-    //     ]);
-
-    //     $response = curl_exec($ch);
-
-    //     if (curl_errno($ch)) {
-    //         throw new Exception(curl_error($ch));
-    //     }
-
-    //     curl_close($ch);
-
-    //     return json_decode($response, true) ?? ['success' => false, 'raw' => $response];
-    // }
+   
 
     public function send(
         string $phone,
